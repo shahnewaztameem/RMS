0a256abc35511eb2535610276d47059911b2b7cb
Shahnewaz Tameem <shahnewaztamim@gmail.com>
2020-03-03 02:41:47 +0600

    Resolved merge conflict by incorporating both suggestions.

--- controllers/login.js
+++ controllers/login.js
@@ -33,7 +33,11 @@ router.post('/', (req, res) => {
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
             // password hasing
+=======
+            
+>>>>>>> parent of 7d3320e... Add files via upload
 =======
             
 >>>>>>> parent of 7d3320e... Add files via upload
